8adf10ef39fb3d33cc2c645410f10325d8100e1f
Redha Nedjari <ned.r@hotmail.fr>
2021-01-30 02:33:20 +0100

    Adding State to songs and data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { useState } from "react";
+// Style
 import "./styles/app.scss";
 // Components
 import Song from "./components/Song";
 import Player from "./components/Player";
+// Data
+import data from "./data";
 
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
