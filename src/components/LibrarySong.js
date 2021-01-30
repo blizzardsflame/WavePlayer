793f9e0f6a40888931af92e1230972ae87424638
Redha Nedjari <ned.r@hotmail.fr>
2021-01-30 19:58:45 +0100

    Highlight selected song from the library

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -5,10 +5,21 @@ const LibrarySong = ({
   id,
   audioRef,
   isPlaying,
+  setSongs,
 }) => {
   //Handler
   const songSelectHandler = async () => {
     await setCurrentSong(song);
+    // Active state
+    const newSongs = songs.map((song) => {
+      if (song.id === id) {
+        return { ...song, active: true };
+      } else {
+        return { ...song, active: false };
+      }
+    });
+    setSongs(newSongs);
+    // Song playing
     if (isPlaying) {
       const playPromise = audioRef.current.play();
       if (playPromise !== undefined) {
@@ -19,7 +30,10 @@ const LibrarySong = ({
     }
   };
   return (
-    <div onClick={songSelectHandler} className="library-song">
+    <div
+      onClick={songSelectHandler}
+      className={`library-song ${song.active ? "selected" : ""}`}
+    >
       <img src={song.cover} alt={song.name} />
       <div className="song-description">
         <h3>{song.name}</h3>
